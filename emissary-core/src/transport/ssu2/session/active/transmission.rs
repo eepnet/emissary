ba89acf103f01828b834f780afffd29d679c3544
Aaro Altonen <a.altonen@hotmail.com>
2025-04-24 19:05:59 +0300

    ssu2: handle acks

--- emissary-core/src/transport/ssu2/session/active/transmission.rs
+++ emissary-core/src/transport/ssu2/session/active/transmission.rs
@@ -111,9 +111,6 @@ struct Segment<R: Runtime> {
     /// When was the packet sent.
     sent: R::Instant,
 
-    /// Packet number.
-    pkt_num: u32,
-
     /// Segment kind.
     ///
     /// Either an unfragmented I2NP message or a fragment of an I2NP message.
@@ -155,8 +152,6 @@ impl<R: Runtime> TransmissionManager<R> {
     /// `message` doesn't find inside an MTU, the iterator yields one `MessageKind::FirstFragment`
     /// and one or more `MessageKind::FollowOnFragment`s.
     pub fn segment(&mut self, message: Message) -> impl Iterator<Item = (u32, MessageKind<'_>)> {
-        // TODO: start timer for resends
-
         if message.serialized_len_short() + SSU2_OVERHEAD <= 1200 {
             let pkt_num = self.next_pkt_num();
 
@@ -164,12 +159,13 @@ impl<R: Runtime> TransmissionManager<R> {
                 pkt_num,
                 Segment {
                     sent: R::now(),
-                    pkt_num,
                     segment: SegmentKind::UnFragmented {
                         message: message.serialize_short(),
                     },
                 },
             );
+
+            // TODO: start timer for resends
 
             // segment must exist since it was just inserted into `segments`
             return vec![(
@@ -192,7 +188,6 @@ impl<R: Runtime> TransmissionManager<R> {
                     pkt_num,
                     Segment {
                         sent: R::now(),
-                        pkt_num,
                         segment: match fragment_num {
                             0 => SegmentKind::FirstFragment {
                                 fragment: fragment.to_vec(),
@@ -224,6 +219,8 @@ impl<R: Runtime> TransmissionManager<R> {
             ));
         }
 
+        // TODO: start timer for resends
+
         packets.into_iter()
     }
 
@@ -232,7 +229,325 @@ impl<R: Runtime> TransmissionManager<R> {
     /// - `ack_through` marks the highest packet that was ACKed.
     /// - `num_acks` marks the number of ACKs below `ack_through`
     /// - `range` contains a `(# of NACK, # of ACK)` tuples
-    pub fn register_ack(&mut self, ack_through: u32, num_acks: u8, ranges: &[(u8, u8)]) {
-        // tracing::error!(target: LOG_TARGET, num_segments = ?self.segments.len());
+    ///
+    /// Start from `ack_through` and mark it and `num_acks` many packet that follow as received and
+    /// if there are any ranges specified, go through them and marked packets as received dropped.
+    /// Packets have not been explicitly NACKed are also considered dropped.
+    pub fn register_ack(&mut self, ack_through: u32, mut num_acks: u8, ranges: &[(u8, u8)]) {
+        (0..=num_acks).for_each(|i| {
+            // TODO: rtt
+            self.segments.remove(&(ack_through.saturating_sub(i as u32)));
+        });
+
+        // first packet in the ranges start at `ack_through - num_acks` and the first acked packet
+        // that can be removed from `segments` starts at `ack_through - num_acks - ranges[0].0`
+        let mut next_pkt = ack_through.saturating_sub(num_acks as u32);
+
+        for (nack, ack) in ranges {
+            next_pkt = next_pkt.saturating_sub(*nack as u32);
+
+            for i in 1..=*ack {
+                next_pkt = next_pkt.saturating_sub(1);
+
+                // TODO: rtt
+                self.segments.remove(&next_pkt);
+            }
+        }
+
+        // TODO: if `segments` is empty, cancel timer
+        // TODO: update window?
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::runtime::mock::MockRuntime;
+
+    #[tokio::test]
+    async fn ack_one_packet() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![1, 2, 3],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 1);
+        assert_eq!(mgr.segments.len(), 1);
+
+        mgr.register_ack(1u32, 0u8, &[]);
+
+        assert!(mgr.segments.is_empty());
+    }
+
+    #[tokio::test]
+    async fn ack_multiple_packets_last_packet_missing() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 3 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 4);
+        assert_eq!(mgr.segments.len(), 4);
+
+        mgr.register_ack(4u32, 2u8, &[]);
+
+        assert_eq!(mgr.segments.len(), 1);
+        assert!(mgr.segments.contains_key(&1));
+    }
+
+    #[tokio::test]
+    async fn ack_multiple_packets_first_packet_missing() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 3 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 4);
+        assert_eq!(mgr.segments.len(), 4);
+
+        mgr.register_ack(3u32, 2u8, &[]);
+
+        assert_eq!(mgr.segments.len(), 1);
+        assert!(mgr.segments.contains_key(&4));
+    }
+
+    #[tokio::test]
+    async fn ack_multiple_packets_middle_packets_nacked() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 3 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 4);
+        assert_eq!(mgr.segments.len(), 4);
+
+        mgr.register_ack(4u32, 0u8, &[(2, 1)]);
+
+        assert_eq!(mgr.segments.len(), 2);
+        assert!(mgr.segments.contains_key(&3));
+        assert!(mgr.segments.contains_key(&2));
+    }
+
+    #[tokio::test]
+    async fn multiple_ranges() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 10 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 11);
+        assert_eq!(mgr.segments.len(), 11);
+
+        mgr.register_ack(11u32, 2u8, &[(3, 2), (1, 2)]);
+
+        assert_eq!(mgr.segments.len(), 4);
+        assert!((6..=8).all(|i| mgr.segments.contains_key(&i)));
+        assert!(mgr.segments.contains_key(&3));
+    }
+
+    #[tokio::test]
+    async fn alternating() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(10u32, 0u8, &[(1, 1), (1, 1), (1, 1), (1, 1), (1, 0)]);
+
+        assert_eq!(mgr.segments.len(), 5);
+        assert!((1..=9).all(|i| if i % 2 != 0 {
+            mgr.segments.contains_key(&i)
+        } else {
+            !mgr.segments.contains_key(&i)
+        }));
+    }
+
+    #[tokio::test]
+    async fn no_ranges() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(10u32, 0u8, &[]);
+
+        assert_eq!(mgr.segments.len(), 9);
+        assert!((1..=9).all(|i| mgr.segments.contains_key(&i)));
+    }
+
+    #[tokio::test]
+    async fn highest_pkts_not_received() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(4u32, 0u8, &[(1, 2)]);
+
+        assert_eq!(mgr.segments.len(), 7);
+        assert!((5..=10).all(|i| mgr.segments.contains_key(&i)));
+        assert!(mgr.segments.contains_key(&3));
+    }
+
+    #[tokio::test]
+    async fn invalid_nack_range() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(10u32, 0u8, &[(2, 0), (2, 0), (2, 0), (2, 0), (1, 0)]);
+
+        assert_eq!(mgr.segments.len(), 9);
+        assert!((1..=9).all(|i| mgr.segments.contains_key(&i)));
+        assert!(mgr.segments.contains_key(&3));
+    }
+
+    #[tokio::test]
+    async fn invalid_ack_range() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(10u32, 0u8, &[(0, 2), (0, 2), (0, 2), (0, 2), (0, 1)]);
+
+        assert!(mgr.segments.is_empty());
+    }
+
+    #[tokio::test]
+    async fn num_acks_out_of_range() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(10u32, 128u8, &[]);
+
+        assert!(mgr.segments.is_empty());
+    }
+
+    #[tokio::test]
+    async fn nacks_out_of_range() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(10u32, 0u8, &[(128u8, 0)]);
+
+        assert_eq!(mgr.segments.len(), 9);
+        assert!((1..=9).all(|i| mgr.segments.contains_key(&i)));
+    }
+
+    #[tokio::test]
+    async fn acks_out_of_range() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(10u32, 0u8, &[(0, 128u8), (128u8, 0u8)]);
+
+        assert!(mgr.segments.is_empty());
+    }
+
+    #[tokio::test]
+    async fn highest_seen_out_of_range() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(1337u32, 10u8, &[]);
+
+        assert_eq!(mgr.segments.len(), 10);
+    }
+
+    #[tokio::test]
+    async fn num_ack_out_of_range() {
+        let mut mgr = TransmissionManager::<MockRuntime>::new(Arc::new(AtomicU32::new(1u32)));
+        let pkts = mgr
+            .segment(Message {
+                payload: vec![0u8; 1200 * 9 + 512],
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(pkts.len(), 10);
+        assert_eq!(mgr.segments.len(), 10);
+
+        mgr.register_ack(15u32, 255, &[]);
+
+        assert!(mgr.segments.is_empty());
     }
 }
